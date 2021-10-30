63d6530f977c60de8a9339ee49f265ea14b7428d
Yu Tao <yutaolife@users.noreply.github.com>
2021-10-30 23:38:54 +0800

    Update run_squad.py
    modify with open function add utf-8

--- run_squad.py
+++ run_squad.py
@@ -879,14 +879,14 @@ def write_predictions(all_examples, all_features, all_results, n_best_size,
 
     all_nbest_json[example.qas_id] = nbest_json
 
-  with tf.gfile.GFile(output_prediction_file, "w") as writer:
+  with open(output_prediction_file, "w", encoding="utf-8") as writer:
     writer.write(json.dumps(all_predictions, indent=4,ensure_ascii=False) + "\n")
 
-  with tf.gfile.GFile(output_nbest_file, "w") as writer:
+  with open(output_nbest_file, "w", encoding="utf-8") as writer:
     writer.write(json.dumps(all_nbest_json, indent=4,ensure_ascii=False) + "\n")
 
   if FLAGS.version_2_with_negative:
-    with tf.gfile.GFile(output_null_log_odds_file, "w") as writer:
+    with open(output_null_log_odds_file, "w", encoding="utf-8") as writer:
       writer.write(json.dumps(scores_diff_json, indent=4,ensure_ascii=False) + "\n")
 
 
